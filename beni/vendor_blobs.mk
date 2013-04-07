26d3052643abfb1d0360785f1809d1954e94a2f7
Conn O'Griofa <connogriofa@gmail.com>
2013-04-07 13:04:52 -0300

    Samsung vendor: add support for kernel 3.0.x & ath6kl driver
    Notes:
     - No common 3.0x fsr/param drivers are provided for other devices
       (since the driver has not yet been fixed for other Samsung devices).
       When it's fixed, place the files in:
       msm7x27-common/proprietary/lib/modules-30x/
     - ath6kl driver doesn't yet distinguish between board data calibration
       revisions; we will copy all revisions in case support is added
       later. Until then, the driver will use the default bdata.bin.
     - added ath6kl hw2.0 fw 3.1.1.149 (same as version in linux-firmware.git)
    
    Change-Id: Idd3a3b19c2ace04ef4434ebea1e359999294eaeb

--- beni/vendor_blobs.mk
+++ beni/vendor_blobs.mk
@@ -18,11 +18,19 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/msm7x27-common/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so
 
 ## Atheros WiFi - board data calibration
+ifeq ($(BOARD_WLAN_DEVICE),ath6kl_compat)
+PRODUCT_COPY_FILES += \
+    vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.01:system/etc/firmware/ath6k/AR6003/hw2.0/bdata.bin.01 \
+    vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.02:system/etc/firmware/ath6k/AR6003/hw2.0/bdata.bin.02 \
+    vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.03:system/etc/firmware/ath6k/AR6003/hw2.0/bdata.bin.03 \
+    vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin:system/etc/firmware/ath6k/AR6003/hw2.0/bdata.bin
+else
 PRODUCT_COPY_FILES += \
     vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.01:system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.01 \
     vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.02:system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.02 \
     vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.03:system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.03 \
     vendor/samsung/beni/proprietary/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin:system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin
+endif
 
 ## Bluetooth
 PRODUCT_COPY_FILES += \
